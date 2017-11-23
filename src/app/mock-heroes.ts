d3fef1e0155ec1180887b1d5cd2514947d038ee1
mitulshah96 <mitulshah96@gmail.com>
2017-11-23 16:03:45 +0530

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mitul' },
+    { id: 12, name: 'vaibhav' },
+    { id: 13, name: 'Kripa' },
+    { id: 14, name: 'Ajay' },
+    { id: 15, name: 'Firoz' },
+];
